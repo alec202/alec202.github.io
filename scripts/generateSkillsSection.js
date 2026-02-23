06ac90a5d6b0723cd60369b32e432a232d472ac9
Alec Mirambeau <alecmirambeau@gmail.com>
2026-02-23 16:36:50 -0500

    on skills section

---
+++ scripts/generateSkillsSection.js
@@ -0,0 +1,15 @@
+class CategorizedSkills {
+    constructor() {
+        this.frontEnd = [];
+        this.backEnd = [];
+        this.tools = [];
+    }
+}
+
+class individualSkill {
+    constructor() {
+        this.name;
+        this.img;
+    }
+}
+const categorizedSkills = CategorizedSkills();
