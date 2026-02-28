00e749e9adfdd913f01383a59267b2259e7aed26
Alec Mirambeau <alecmirambeau@gmail.com>
2026-02-28 18:17:18 -0500

    need to add scrabble game for kotlin to projects section and handle on click listeners using javascript

---
+++ scripts/projects-view-controller.js
@@ -0,0 +1,10 @@
+const allProjects = document.querySelectorAll("project-card");
+
+for (const project of allProjects) {
+    // need to make it so whenever clicking on a project, it will
+    // open the link to the github, or to the
+    // web app if hosted.
+    project.addEventListener("click" () => {
+        
+    })
+}
