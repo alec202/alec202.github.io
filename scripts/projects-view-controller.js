1c36a3b026577d89b8cc06f10cf7adcd88e3df36
Alec Mirambeau <alecmirambeau@gmail.com>
2026-02-28 18:46:02 -0500

    started case statement b4 break

--- scripts/projects-view-controller.js
+++ scripts/projects-view-controller.js
@@ -1,10 +1,17 @@
-const allProjects = document.querySelectorAll("project-card");
+const allProjects = document.querySelectorAll(".project-card");
 
+let link = "";
 for (const project of allProjects) {
     // need to make it so whenever clicking on a project, it will
     // open the link to the github, or to the
     // web app if hosted.
+    
     project.addEventListener("click", (eve) => {
         console.log(project);
     })
+    switch (project.id) {
+        case "vue-product-listing":
+            link = "https://gvsu-cis371.github.io/project5-teamteam/";
+            break;
+    }
 }
