58d334fae6183e488ea78e0d1efc41c92c7c8c65
Alec Mirambeau <alecmirambeau@gmail.com>
2026-02-28 21:52:35 -0500

    adding click listeners and rest of projects

--- scripts/projects-view-controller.js
+++ scripts/projects-view-controller.js
@@ -1,17 +1,11 @@
-const allProjects = document.querySelectorAll(".project-card");
+// need to make it so whenever clicking on a project, it will
+// open the link to the github, or to the
+// web app if hosted.
 
-let link = "";
-for (const project of allProjects) {
-    // need to make it so whenever clicking on a project, it will
-    // open the link to the github, or to the
-    // web app if hosted.
-    
-    project.addEventListener("click", (eve) => {
-        console.log(project);
-    })
-    switch (project.id) {
-        case "vue-product-listing":
-            link = "https://gvsu-cis371.github.io/project5-teamteam/";
-            break;
-    }
-}
+const productListingPage = document.querySelector("#vue-product-listing");
+productListingPage.addEventListener("click", () => document.location.href = "https://gvsu-cis371.github.io/project5-teamteam/");
+
+const movieReviewer = document.querySelector("#movie-reviewer")
+movieReviewer.addEventListener("click", () => document.location.href = "https://github.com/alec202/movieReviewerApp");
+
+const storeDatabaseManagementSystem = document.querySelector("#store-database-management-system");
