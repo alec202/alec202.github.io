2cc67d02072e6349544a14b4ff6c6fe38c5a23e3
Alec Mirambeau <alecmirambeau@gmail.com>
2026-04-06 10:31:52 -0400

    added inert to propeties

--- scripts/nav-menu.js
+++ scripts/nav-menu.js
@@ -1,5 +1,22 @@
 const openNavBttn = document.getElementById("header--open-nav");
 const closeNavBttn = document.getElementById("header--close-nav");
+const mediaIsMobile = window.matchMedia("(width > 414px)")
+const headerOptions = document.getElementById("header--options");
+
+function setupTopNav(media) {
+    if (media.matches) {
+        // is Desktop/tablet screen
+        console.log("is desktop/tablet")
+        headerOptions.removeAttribute("inert");
+
+    } else {
+        // is phone screen
+        console.log("mobileScreen");
+        headerOptions.setAttribute("inert", "");
+
+    }
+
+}
 
 function toggleNavOpen() {
     openNavBttn.setAttribute("aria-expanded", "true");
@@ -12,3 +29,5 @@ function toggleNavClosed() {
 openNavBttn.addEventListener("click", toggleNavOpen);
 
 closeNavBttn.addEventListener("click", toggleNavClosed);
+
+setupTopNav(mediaIsMobile)
