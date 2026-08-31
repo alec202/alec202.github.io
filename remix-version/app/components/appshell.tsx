48f5ec2c1370cef6e88aa2ea2af975053c981a00
alec202 <alecmirambeau@gmail.com>
2026-08-30 20:45:25 -0400

    switched to remix v2

---
+++ remix-version/app/components/appshell.tsx
@@ -0,0 +1,13 @@
+import {AppShell} from "@mantine/core";
+
+export default function Appshell(){
+
+    return (
+        <Appshell>
+            <AppShell.Header>
+                
+            </AppShell.Header>
+        </Appshell>
+    )
+
+}
---
+++ remix-version/app/components/header.tsx
@@ -0,0 +1,16 @@
+import { Group } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+
+
+export default function Header(){
+    const [opened, {open: openPageTabs, close: closePageTabs}] = useDisclosure();
+
+    return (
+        <Group>
+            
+        </Group>
+    )
+
+
+
+}
